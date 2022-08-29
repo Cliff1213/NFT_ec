d27637339b7c0c56a684c882ca85c333a53debb8
Cliff Chu <wave99487chu@gmail.com>
2022-08-29 17:54:24 +0800

    Update 2022-08-29T09:54:22.750Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,14 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var btnSearch = document.querySelector('.btn-search-mobile');
+var inputSearch = document.querySelector('.input-search-mobile');
+var btnBack = document.querySelector('.btn-back');
+var navCollapse = document.querySelector('.navbar-collapse');
+btnSearch.addEventListener('click', function () {
+  navCollapse.classList.remove('show');
+  inputSearch.classList.remove('d-none');
+});
+btnBack.addEventListener('click', function () {
+  inputSearch.classList.add('d-none');
 });
 //# sourceMappingURL=all.js.map
