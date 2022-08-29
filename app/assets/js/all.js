64ede20ebefb34f61e14cd3ac014b32a7794fe10
Cliff Chu <wave99487chu@gmail.com>
2022-08-29 17:53:20 +0800

    navbar finish

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,13 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+const btnSearch = document.querySelector('.btn-search-mobile');
+const inputSearch = document.querySelector('.input-search-mobile');
+const btnBack = document.querySelector('.btn-back');
+const navCollapse = document.querySelector('.navbar-collapse');
+
+btnSearch.addEventListener('click', () => {
+  navCollapse.classList.remove('show');
+  inputSearch.classList.remove('d-none');
+})
+
+btnBack.addEventListener('click', () => {
+  inputSearch.classList.add('d-none');
+})
