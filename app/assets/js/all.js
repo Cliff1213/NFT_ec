4d9b4357811b7295ba4da7621a7413c5affc9df6
Cliff Chu <wave99487chu@gmail.com>
2022-09-02 00:14:52 +0800

    debug

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -12,8 +12,29 @@ btnBack.addEventListener('click', () => {
   inputSearch.classList.add('d-none');
 })
 
+// swiper
+const swiper = new Swiper(".artist-swiper", {
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 1,
+  breakpoints: {
+  576: {
+    slidesPerView: 2
+  },
+  992: {
+    slidesPerView: 3
+  }
+  },
+  spaceBetween: 24,
+  effect: 'slide',
+  pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+  },
+});
 
 
+// masonry
 $('.artwork-list').imagesLoaded().progress( function() {
   $('.artwork-list').masonry(); 
 });
--- app/assets/js/swiper.js
+++ app/assets/js/swiper.js
@@ -3,17 +3,18 @@
 //   centeredSlides: true,
 //   slidesPerView: 1,
 //   breakpoints: {
-//     576: {
-//       slidesPerView: 2
-//     },
-//     992: {
-//       slidesPerView: 3
-//     }
+//   576: {
+//     slidesPerView: 2
+//   },
+//   992: {
+//     slidesPerView: 3
+//   }
 //   },
 //   spaceBetween: 24,
 //   effect: 'slide',
 //   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
+//   el: ".swiper-pagination",
+//   clickable: true,
 //   },
 // });
+
