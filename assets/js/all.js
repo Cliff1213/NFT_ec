209df28fb2e4603eaf61a9c1c6807fbf1fc33b13
Cliff Chu <wave99487chu@gmail.com>
2022-09-02 23:46:01 +0800

    Update 2022-09-02T15:45:58.227Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// navbar
 var btnSearch = document.querySelector('.btn-search-mobile');
 var inputSearch = document.querySelector('.input-search-mobile');
 var btnBack = document.querySelector('.btn-back');
@@ -18,7 +19,7 @@ var swiper = new Swiper(".artist-swiper", {
   centeredSlides: true,
   slidesPerView: 1,
   breakpoints: {
-    576: {
+    768: {
       slidesPerView: 2.5
     },
     992: {
@@ -36,24 +37,4 @@ var swiper = new Swiper(".artist-swiper", {
 $('.artwork-list').imagesLoaded().progress(function () {
   $('.artwork-list').masonry();
 });
-// const swiper = new Swiper(".artist-swiper", {
-//   loop: true,
-//   centeredSlides: true,
-//   slidesPerView: 1,
-//   breakpoints: {
-//   576: {
-//     slidesPerView: 2
-//   },
-//   992: {
-//     slidesPerView: 3
-//   }
-//   },
-//   spaceBetween: 24,
-//   effect: 'slide',
-//   pagination: {
-//   el: ".swiper-pagination",
-//   clickable: true,
-//   },
-// });
-"use strict";
 //# sourceMappingURL=all.js.map
